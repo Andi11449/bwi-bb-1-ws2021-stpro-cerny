8f264a4b69cae134827d79e5e475d88645d81c1d
cerny <cerny@technikum-wien.at>
2021-09-14 20:31:04 +0200

    class 2 code - past 1

---
+++ src/class2_variables_and_selection/Milch.java
@@ -0,0 +1,17 @@
+package class2_variables_and_selection;
+
+import java.util.Scanner;
+
+public class Milch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Fettgehalt: ");
+        int fett = sc.nextInt();
+        System.out.print("Haltbar: ");
+        int haltbar = sc.nextInt();
+
+
+
+        sc.close();
+    }
+}
