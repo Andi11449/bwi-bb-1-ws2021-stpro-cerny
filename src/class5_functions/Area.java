7e3513d2d380494c7b53bf0a338d16d5c7f14ccb
cerny <cerny@technikum-wien.at>
2021-10-05 18:50:56 +0200

    added Area demo for class 5 functions

---
+++ src/class5_functions/Area.java
@@ -0,0 +1,35 @@
+package class5_functions;
+
+public class Area {
+    public static void main(String[] args){
+        int width = 2;
+        int height = 3;
+        double width2 = 2.0;
+        double height2 = 3.5;
+        double radius = 3.0;
+
+        //calls the first function because width and height are int
+        double area = calculateArea(width, height);
+        System.out.println(area);
+
+        //calls the second function because width2 and height2 are double
+        area = calculateArea(width2, height2);
+        System.out.println(area);
+
+        //calls the third function because only one double argument is passed
+        area = calculateArea(radius);
+        System.out.println(area);
+    }
+
+    public static double calculateArea(int width, int height){
+        return (double) width * height;
+    }
+
+    public static double calculateArea(double width, double height){
+        return width * height;
+    }
+
+    public static double calculateArea(double radius){
+        return 2 * radius * Math.PI;
+    }
+}
