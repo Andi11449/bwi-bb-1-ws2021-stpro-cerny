8bb801f166b671fa13eb1911da082bef37b8e414
cerny <cerny@technikum-wien.at>
2021-10-11 17:30:36 +0200

    added self checks Arrays I, sample solution for MatchstickGame and basis for recursive HandshakeProblem

---
+++ src/review_class3/MatchstickGame.java
@@ -0,0 +1,56 @@
+package review_class3;
+
+import java.util.Scanner;
+
+public class MatchstickGame {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to the matchstick game!\nDo you want to play against a computer? (y/n)");
+
+        boolean computer = sc.nextLine().equals("y");
+
+        int sticks = 0;
+        while (sticks < 20 || sticks > 100) {
+            System.out.println("How many sticks to start (between 20 and 100): ");
+            sticks = sc.nextInt();
+        }
+
+        boolean isPlayerOneTurn = true;
+
+        while (sticks > 0) {
+            System.out.println("\nSticks left: " + sticks);
+            for (int i = 1; i <= sticks; i++) {
+                System.out.print("|");
+                if(i % 5 == 0)
+                    System.out.print(" ");
+            }
+            System.out.println();
+
+            while (true) {
+                System.out.printf("\nPlayer %d take 1-3 sticks: ", isPlayerOneTurn ? 1 : 2);
+                int n;
+                if(isPlayerOneTurn || computer == false) {
+                    n = sc.nextInt();
+                }
+                else {
+                    n = (sticks - 1) % 4;
+                    if (n == 0)
+                        n = 3;
+                    System.out.println(n);
+                }
+
+                if (n > 0 && n < 4) {
+                    sticks -= n;
+                    break;
+                }
+            }
+
+            isPlayerOneTurn = !isPlayerOneTurn;
+        }
+
+        System.out.printf("\nPlayer %d wins!", isPlayerOneTurn ? 1 : 2);
+
+        sc.close();
+    }
+}
---
+++ src/self_study_f_arrays/arrays/PrintArray.java
@@ -0,0 +1,13 @@
+package self_study_f_arrays.arrays;
+
+import java.awt.*;
+
+public class PrintArray {
+    public static void main(String[] args) {
+        int a[] = {4, 5, 7, 2, 3 ,9, 6, 4, 3, 5};
+
+        for(int i=0; i < a.length; i++) {
+            System.out.printf("[%d]: %d\n", i, a[i]);
+        }
+    }
+}
