03d8b3d7d4ac5d4f86de8c23dcd50a732f9cc870
cerny <cerny@technikum-wien.at>
2021-11-10 18:33:47 +0100

    added output for binary/linear SearchDemo

--- src/class9_sort_and_search/SearchDemo.java
+++ src/class9_sort_and_search/SearchDemo.java
@@ -6,24 +6,43 @@ public class SearchDemo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = 10;
-        int[] forward = getArray(n, "linear");
-        print(forward);
-        int idx = binarySearch(forward, 9);
-        //print(reversed);
-        //print(random);
+        //int n = 10;
+        //int[] forward = getArray(n, "linear");
+
+        int[] arr = new int[] {1, 2, 4, 5, 7, 11, 12, 33, 45, 77};
+        print(arr);
+        int idx = binarySearch(arr, 45);
+        //System.out.println(idx);
+        idx = linearSearch(arr, 45);
+        //System.out.println(idx);
+
+
 
         sc.close();
     }
 
+    public static int linearSearch(int sortedArray[], int x) {
+        for(int i=0; i < sortedArray.length; i++){
+            if(sortedArray[i] == x){
+                System.out.printf("Linear Search: Found %d at index %d after %d comparisons.\n", x, i, i);
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public static int binarySearch(int sortedArray[], int x) {
+        int count = 0;
         int l = 0;
         int r = sortedArray.length - 1;
         while (l <= r) {
+            count++;
             int m = l + (r - l) / 2;
             // Check if value x is found at position m
-            if (sortedArray[m] == x)
+            if (sortedArray[m] == x){
+                System.out.printf("Binary Search: Found %d at index %d after %d comparisons.\n", x, m, count);
                 return m;
+            }
             // If x is greater, ignore left half
             if (sortedArray[m] < x){
                 l = m + 1;
