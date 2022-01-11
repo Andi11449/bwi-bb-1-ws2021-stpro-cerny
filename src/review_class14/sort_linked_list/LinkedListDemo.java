b2c085bc33384dd006ad51c974fd8d3d14941b25
cerny <cerny@technikum-wien.at>
2022-01-11 18:02:56 +0100

    added implementation for all sort algorithms with LinkedList in review_class14 -> LinkedListDemo

--- src/review_class14/sort_linked_list/LinkedListDemo.java
+++ src/review_class14/sort_linked_list/LinkedListDemo.java
@@ -13,16 +13,41 @@ public class LinkedListDemo {
         insert(list, 13, 1);
         addBack(list, 13);
 
-        print(list);
+        System.out.println("Bubble Sort:");
+        //create copy so that original list will not be sorted
+        LinkedList copy = copy(list);
+        print(copy);
+        bubble(copy);
+        print(copy);
 
-        insertion(list);
+        System.out.println("Insertion Sort:");
+        copy = copy(list);
+        print(copy);
+        insertion(copy);
+        print(copy);
 
-        print(list);
+        System.out.println("Selection Sort:");
+        copy = copy(list);
+        print(copy);
+        selection(copy);
+        print(copy);
 
+        System.out.println("Merge Sort:");
+        copy = copy(list);
+        print(copy);
+        mergesort(copy);
+        print(copy);
 
-
+        System.out.println("Quick Sort:");
+        copy = copy(list);
+        print(copy);
+        quicksort(copy);
+        print(copy);
 
     }
+
+
+    /*** Sort algorithms ***/
 
     public static void bubble(LinkedList list) {
         boolean swapped;
@@ -33,11 +58,12 @@ public class LinkedListDemo {
                 //if (list[j - 1] > list[j]) {
                 ListNode first = getNode(list, j - 1);
                 ListNode second = getNode(list, j);
-                if (first.data < second.data) {
-                    //swap(list, j - 1, j);
-                    int tmp = first.data;
-                    first.data = second.data;
-                    second.data = tmp;
+                if (first.data > second.data) {
+                    swap(list, j - 1, j);
+                    /* swap implemented in bubble function */
+                    // int tmp = first.data;
+                    // first.data = second.data;
+                    // second.data = tmp;
                     swapped = true;
                 }
             }
@@ -61,14 +87,121 @@ public class LinkedListDemo {
         }
     }
 
-    /*public static void swap(LinkedList list, int i, int j) {
+    public static void selection(LinkedList list) {
+        int i, j;
+        for (i = 0; i < list.size - 1; i++) {
+            int min_idx = i;
+            /* find idx of minimum of unsorted partition */
+            for (j = i + 1; j < list.size; j++) {
+                if (getNode(list, j).data < getNode(list, min_idx).data)
+                    min_idx = j;
+            }
+            /* swap minimum to correct position */
+            swap(list, i, min_idx);
+        }
+    }
+
+    public static void swap(LinkedList list, int i, int j) {
         ListNode first = getNode(list, i);
         ListNode second = getNode(list, j);
 
         int tmp = first.data;
         first.data = second.data;
         second.data = tmp;
-    }*/
+    }
+
+    public static void mergesort(LinkedList list) {
+        /* create an auxilliar array that is used be the merge function */
+        int[] aux = new int[list.size];
+        /* starts the mergesort by calling the recursive function mSort */
+        mSort(list, 0, list.size - 1, aux);
+    }
+
+    public static void mSort(LinkedList list, int left, int right, int[] aux) {
+        /* stops the recursion when portion can't be splited further */
+        if (right <= left) return;
+        /* call mSort recursively with the left and the right split */
+        int middle = (right + left) / 2;
+        mSort(list, left, middle, aux);
+        mSort(list, middle + 1, right, aux);
+        /* merge is first executed for the smallest splits */
+        merge(list, left, middle, right, aux);
+    }
+
+    public static void merge(LinkedList list, int left, int middle, int right, int[] aux) {
+        /* fill the helper array with the list between left and right */
+        for (int i = left; i <= right; i++)
+            aux[i] = getNode(list, i).data;
+        /* the l counter starts at the left position and goes till the middle */
+        int l = left;
+        /* the r counter starts at the position middle+1 and goes till right position*/
+        int r = middle+1;
+        /* sorts the elements in the list array */
+        for (int i = left; i <= right; i++){
+            /* if the left element is smaller take it otherwise take */
+            /* the right one and increase the respective counter */
+            if (r > right || (l <= middle && aux[l] < aux[r]))
+                getNode(list, i).data = aux[l++];
+            else
+                getNode(list, i).data = aux[r++];
+        }
+    }
+
+
+    public static void quicksort(LinkedList list) {
+        /* starts the quicksort algorithm */
+        quicksort(list, 0, list.size-1);
+    }
+
+    public static void quicksort(LinkedList list, int left, int right) {
+        if (right<=left)
+            return;
+        /* partition returns the new position of the pivot element */
+        int i=partition(list, left, right);
+        //print(numbers);
+        /* quicksort is then executed again for the left part ... */
+        quicksort(list, left, i-1);
+        /* ... and for the right part */
+        quicksort(list, i+1, right);
+    }
+
+    public static int partition(LinkedList list, int left, int right) {
+        /* sorting an array of length 1 is trivial */
+        if (left == right) return left;
+        /* the right element is selected as pivot */
+        int pivot = getNode(list, right).data;
+        int i = left - 1, j = right; /* search indices from left and right */
+        while (true) {
+            /* search for the first element bigger then pivot from the left */
+            while (getNode(list, ++i).data < pivot)
+                ;
+            /* search for the first element smaller then pivot from the right */
+            while (pivot < getNode(list, --j).data)
+                if (j == left) /* stop at left search index */
+                    break;
+            /* pivot positon is found and the loop is stopped */
+            if (i >= j)
+                break;
+            /* swap the bigger element on the left side with */
+            /* the smaller element on the right side */
+            swap(list, i, j);
+        }
+        /* swap the pivot element */
+        swap(list, i, right);
+        /* return the new position of the pivot element */
+        return i;
+    }
+
+
+    /*** LinkedList operations***/
+
+    private static LinkedList copy(LinkedList list) {
+        LinkedList copy = new LinkedList();
+        for(ListNode cur = list.head; cur != null; cur = cur.next){
+            addBack(copy, cur.data);
+        }
+        return copy;
+    }
 
     private static void setValue(LinkedList list, int value, int pos) {
         ListNode node = getNode(list, pos);
